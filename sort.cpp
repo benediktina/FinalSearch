bbb17b291fcd78cd3c3b65bf720c54daa5692743
PutnaArvydas <putnaarvydas@gmail.com>
2013-11-27 21:05:27 +0200

    Veikiantis mergesort

--- sort.cpp
+++ sort.cpp
@@ -5,19 +5,32 @@ template <class T> void compexch(T &, T &);
 template <class T> void selection(T *, int, int);
 template <class T> void insertion(T *, int, int);
 template <class T> void bubble(T *, int, int);
+template <class T> void marge(T *, int, int);
 using namespace std;
 int main(int argc, char *argv[])
 {
-    int i, N = atoi(argv[1]), KaDaryt = atoi(argv[2]);
+    //int i, N = atoi(argv[1]), KaDaryt = atoi(argv[2]);
+	int i, N , KaDaryt=1;
+    int kuris;
+    cin>>N; cin>>kuris;
     int *a = new int[N];
-    if (KaDaryt) // Atsitiktinai sugeneruoja N skaičių intervale: 1,...,1000.
+	cout<<endl<<"N, kuris"<<endl;
+
+    if (KaDaryt==1) // Atsitiktinai sugeneruoja N skaičių intervale: 1,...,1000.
       for (i = 0; i < N; i++)
         a[i] = 1000*(1.0*rand()/RAND_MAX);
     else // Savo nuožiūra įvedame skaičius. Norėdami baigti įvedimą, įvedame bet kokį simbolį - ne skaičių.
       { N = 0; while (cin >> a[N]) N++; }
+
     cout << "Įvestas skaičių masyvas yra:" << endl;
     for (i = 0; i < N; i++) cout << a[i] << " ";
     cout << endl;
+
+	if(kuris == 1)selection(a, 0, N-1);
+    else if (kuris == 2) insertion (a, 0, N-1);
+    else if (kuris == 3) bubble (a, 0, N-1);
+    else marge(a, 0, N-1);
+
     selection(a, 0, N-1);
     cout << "Surūšiuotas skaičių masyvas yra:" << endl;
     for (i = 0; i < N; i++) cout << a[i] << " ";
@@ -60,3 +73,60 @@ void bubble(T a[], int l, int r)
       for (int j = r; j > i; j--)
         compexch(a[j-1], a[j]);
   }
+  template <class T>
+void marge(T a[], int l, int r)
+{
+ int i = 0;
+ int length = r - l + 1;
+ int pivot  = 0;
+ int merge1 = 0;
+ int merge2 = 0;
+ //int temp[100];
+ int *temp = new int[r];
+
+ if(l == r)
+ return;
+
+ pivot  = (l + r) / 2;
+
+ marge(a, l, pivot);
+ marge(a, pivot + 1, r);
+
+ for(i = 0; i < length; i++)
+ {
+  temp[i] = a[l + i];
+ }
+
+ merge1 = 0;
+ merge2 = pivot - l + 1;
+
+ for(i = 0; i < length; i++)
+ {
+  if(merge2 <= r - l)
+  {
+   if(merge1 <= pivot - l)
+   {
+    if(temp[merge1] > temp[merge2])
+    {
+     a[i + l] = temp[merge2++];
+    }
+
+    else
+    {
+     a[i + l] = temp[merge1++];
+    }
+   }
+
+   else
+   {
+    a[i + l] = temp[merge2++];
+   }
+  }
+
+  else
+  {
+   a[i + l] = temp[merge1++];
+  }
+ }
+}
+
